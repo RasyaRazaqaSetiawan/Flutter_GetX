baaeabdc6ca79b0a9b5d064c03969bfd0a54a485
Rasya <rasyarazaqasetiawan@gmail.com>
2025-01-22 19:29:57 +0700

    Create Profile Page & Create Logout

--- lib/app/utils/api.dart
+++ lib/app/utils/api.dart
@@ -12,4 +12,6 @@ class BaseUrl {
       'https://praujikom.smkassalaambandung.sch.id/api/events';
   static String deleteEvents =
       'https://praujikom.smkassalaambandung.sch.id/api/events/';
+  static String profile =
+      'https://praujikom.smkassalaambandung.sch.id/api/profile';
 }
